7647969b8242838aece84ba2a1cb626b09af8a15
tech-sam <sumit.s7325@gmail.com>
2020-09-26 12:36:13 +0530

    social link updated

--- src/components/socialIconLinks.js
+++ src/components/socialIconLinks.js
@@ -4,31 +4,31 @@ import IconGithub from './images/github'
 import IconLinkedIn from './images/in'
 import IconTwitter from './images/twitter'
 import IconInstagram from './images/instagram'
-import IconCodepen from './images/codepen'
-import IconCodewars from './images/codewars'
+import Dev from './images/dev'
+import Medium from './images/medium'
 
 
 const SocialIconLinks = ({ iconClasses }) => {
 
   return (
     <>
-      <a href="https://github.com/cadekynaston" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/tech-sam" target="_blank" rel="noopener noreferrer">
         <IconGithub classes={`${iconClasses}`} />
       </a>
-      <a href="https://www.linkedin.com/in/cadekynaston/" target="_blank" rel="noopener noreferrer">
-        <IconLinkedIn classes={`${iconClasses}`}  />
+      <a href="https://www.linkedin.com/in/techsam/" target="_blank" rel="noopener noreferrer">
+        <IconLinkedIn classes={`${iconClasses}`} />
       </a>
-      <a href="https://twitter.com/cadekynaston" target="_blank" rel="noopener noreferrer">
-        <IconTwitter classes={`${iconClasses}`}  />
+      <a href="https://dev.to/tech_sam/" target="_blank" rel="noopener noreferrer">
+        <Dev classes={`${iconClasses}`} />
       </a>
-      <a href="https://www.instagram.com/cadekynaston" target="_blank" rel="noopener noreferrer">
-        <IconInstagram classes={`${iconClasses}`}  />
+      <a href="https://medium.com/@tech_sam" target="_blank" rel="noopener noreferrer">
+        <Medium classes={`${iconClasses}`} />
       </a>
-      <a href="https://codepen.io/cadekynaston/" target="_blank" rel="noopener noreferrer">
-        <IconCodepen classes={`${iconClasses}`}  />
+      <a href="https://twitter.com/_tech_sam" target="_blank" rel="noopener noreferrer">
+        <IconTwitter classes={`${iconClasses}`} />
       </a>
-      <a href="https://codewars.com/users/ck1" target="_blank" rel="noopener noreferrer">
-        <IconCodewars classes={`${iconClasses}`}  />
+      <a href="https://www.instagram.com/the_tech_sam/" target="_blank" rel="noopener noreferrer">
+        <IconInstagram classes={`${iconClasses}`} />
       </a>
     </>
   )
